8d283fae556325946962f978c2eb71f2afe379c1
Vašek Potoček <vasek.potocek@post.cz>
2016-08-08 12:14:37 +0100

    Switch RNG: ranlux48 → minstd_rand

--- genetic.h
+++ genetic.h
@@ -13,10 +13,10 @@ namespace gen {
 /** \brief The default random number generator for Population::rankSelect().
  *
  * A strong RNG is not a necessity for genetic applications, so speed was main
- * preference in choosing `ranlux48`. Can be accessed freely by applications
+ * preference in choosing `minstd_rand`. Can be accessed freely by applications
  * of the framework. Similarly, alternative RNGs may be provided to
  * Population::rankSelect(). */
-static thread_local std::ranlux48_base rng{std::random_device()()};
+static thread_local std::minstd_rand rng{std::random_device()()};
 
 /* Helpers for Population::rankSelect<std::exp> */
 namespace internal {
